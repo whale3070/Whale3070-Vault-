a3c213b4c5d6774e21ec7dcd4dde0a5947fe2da0
whale3070 <WeaponMaster3070@gmail.com>
2026-01-10 23:51:54 +0800

    Update license information in QuickNFT.sol
    Replaced SPDX license identifier with copyright notice.

--- QuickNFT.sol
+++ QuickNFT.sol
@@ -1,4 +1,9 @@
-// SPDX-License-Identifier: MIT
+/*
+ * 版权所有 (C) [2026] [whale3070/ Whale-Valut-NFT团队]
+ * 本项目基于 CC BY-NC 4.0 协议开源，禁止第三方商用（详见仓库 LICENSE 文件）。
+ * 著作权人保留本项目的商业使用权。
+ */
+
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
